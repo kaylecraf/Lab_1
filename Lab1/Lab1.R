67fb27bed1eac840c94659cf91937dea56f276e2
Kaylie Craft <kcraft5@kent.edu>
2025-02-06 16:11:10 -0500

    werking

--- Lab1/Lab1.R
+++ Lab1/Lab1.R
@@ -32,4 +32,46 @@ d.counties %>%
   summarise(stateLandArea = sum(ALAND10))
 
 #plots:p
+d.counties %>% 
+  ggplot(., aes(x = ALAND10, y = ALAND10)) +
+  geom_boxplot(aes(fill = STATEFP10))
+
+#spatial operations
+#look at CRS
+d.counties %>% sf::st_crs()
+d.stations %>% sf::st_crs()
+d.counties %>% sf::st_crs() == d.stations %>% sf::st_crs()
+
+#only counties in delaware
+del.counties <- d.counties %>% dplyr::filter(STATEFP10==10)
+del.stations <- sf::st_intersection(d.stations,del.counties)
+glimpse(del.stations)
+
+
+#Task One!!
+#1.1
+d.counties <- d.counties %>% dplyr::group_by(STATEFP10) %>% mutate(stateArea = sum(ALAND10+AWATER10))
+d.counties <- d.counties %>% mutate(LandAreaPrecent = (ALAND10/stateArea)*100)
+
+#1.2
+d.counties <- d.counties %>%
+  mutate(CountyArea = (ALAND10+AWATER10))
+d.counties <- d.counties %>%
+  mutate(WaterProp = (AWATER10/CountyArea))
+Max.Water <- d.counties %>%
+  dplyr::group_by(STATEFP10) %>%
+  slice_max(WaterProp)
+
+#1.3
+d.counties %>%
+  dplyr::group_by(STATEFP10) %>%
+  dplyr::count(STATEFP10)
+
+#1.4
+d.stations <- d.stations %>% mutate(length = (nchar(STATION_NA)))
+d.stations %>% slice_min(length)
+
+
+
+
 
