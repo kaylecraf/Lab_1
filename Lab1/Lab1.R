41e907d103435156dd1eac22f32ea8dfa9001c04
Kaylie Craft <kcraft5@kent.edu>
2025-02-06 12:35:15 -0500

    werk

--- Lab1/Lab1.R
+++ Lab1/Lab1.R
@@ -15,3 +15,21 @@ d.counties %>% sf::st_is_valid()
 d.counties <- d.counties %>% sf::st_make_valid()
 d.counties %>% dplyr::select(GEOID10, ALAND10) %>% head()
 d.counties %>% dplyr::select(-NAME10) %>% head()
+#only shows the columns specified
+d.counties %>% dplyr::select(GEOID10:CLASSFP10) %>% head()
+#shows all columns except for the ones in this range
+d.counties %>% dplyr::select(-(GEOID10:CLASSFP10)) %>% head()
+#shows columns that have headings that start with C
+d.counties %>% dplyr::select(starts_with("C")) %>% head()
+
+#Grouping Stuff
+d.counties %>% dplyr::group_by(STATEFP10) %>% mutate(stateLandArea = sum(ALAND10))
+
+#convert to tibble and get the stuff we want
+d.counties %>%
+  as_tibble() %>% dplyr::select(-geometry) %>%
+  group_by(STATEFP10) %>%
+  summarise(stateLandArea = sum(ALAND10))
+
+#plots:p
+
